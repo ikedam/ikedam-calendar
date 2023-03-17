c5520eb4e20a3b0be5dd875fc724b5bdf4f8df28
ikedam <devld@ikedam.jp>
2023-03-17 10:49:08 +0900

    Prepare dynamic calendar

--- calendar.js
+++ calendar.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const req = new XMLHttpRequest();
-  req.withCredentials = false;
-  req.addEventListener('load', () => {
-    const holidays = JSON.parse(req.responseText);
+(() => {
+  let holidays = undefined;
+  let markHolidays = () => {
+    if (!holidays) {
+      return;
+    }
     document.querySelectorAll('.cells.days>div').forEach((e) => {
       let d = e.innerText;
       if (!d) {
@@ -13,7 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
         e.classList.add('holiday');
       }
     });
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const req = new XMLHttpRequest();
+    req.withCredentials = false;
+    req.addEventListener('load', () => {
+      holidays = JSON.parse(req.responseText);
+      markHolidays();
+    });
+    req.open('GET', 'https://holidays-jp.github.io/api/v1/date.json');
+    req.send();
   });
-  req.open('GET', 'https://holidays-jp.github.io/api/v1/date.json');
-  req.send();
-});
+})();
